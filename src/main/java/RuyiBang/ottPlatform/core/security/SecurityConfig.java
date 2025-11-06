290478a78ee0519dd9e2d1c2ce03d9ee563e04ef
toothpick <madhur.sarath@gmail.com>
2025-11-05 17:00:03 -0700

    updates

--- src/main/java/RuyiBang/ottPlatform/core/security/SecurityConfig.java
+++ src/main/java/RuyiBang/ottPlatform/core/security/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login", "/public/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/_admin/h2/**").hasRole("ADMIN")
+                        .requestMatchers("/auth/register", "/auth/login", "/public/**").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
